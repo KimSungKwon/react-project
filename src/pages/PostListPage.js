20e7b3ebe6c06ff38bd4fed27e02b01499c63693
KimSungKwon <sungkwonn@naver.com>
2020-06-09 16:16:19 +0900

    Upload front-end project

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import WriteButton from '../components/common/WriteButton';
+
+const PostListPage = () => {
+    return (
+        <>
+            <HeaderContainer />
+            <WriteButton />
+            <div>안녕하세요</div>
+        </>
+    )
+};
+
+export default PostListPage;
